3bbd8d33c16ee77470442fdf0e59a25a128ad253
kvbolashikova18 <kvbolashikova18@codingburgas.bg>
2021-07-05 15:03:58 +0300

    Create database and user table

---
+++ Team Management.sql
@@ -0,0 +1,96 @@
+USE [master]
+GO
+/****** Object:  Database [Team Management]    Script Date: 7/5/2021 2:54:14 PM ******/
+CREATE DATABASE [Team Management]
+ CONTAINMENT = NONE
+ ON  PRIMARY 
+( NAME = N'Team Management', FILENAME = N'C:\Users\kvbolashikova18\Team Management.mdf' , SIZE = 8192KB , MAXSIZE = UNLIMITED, FILEGROWTH = 65536KB )
+ LOG ON 
+( NAME = N'Team Management_log', FILENAME = N'C:\Users\kvbolashikova18\Team Management_log.ldf' , SIZE = 8192KB , MAXSIZE = 2048GB , FILEGROWTH = 65536KB )
+GO
+IF (1 = FULLTEXTSERVICEPROPERTY('IsFullTextInstalled'))
+begin
+EXEC [Team Management].[dbo].[sp_fulltext_database] @action = 'enable'
+end
+GO
+ALTER DATABASE [Team Management] SET ANSI_NULL_DEFAULT OFF 
+GO
+ALTER DATABASE [Team Management] SET ANSI_NULLS OFF 
+GO
+ALTER DATABASE [Team Management] SET ANSI_PADDING OFF 
+GO
+ALTER DATABASE [Team Management] SET ANSI_WARNINGS OFF 
+GO
+ALTER DATABASE [Team Management] SET ARITHABORT OFF 
+GO
+ALTER DATABASE [Team Management] SET AUTO_CLOSE OFF 
+GO
+ALTER DATABASE [Team Management] SET AUTO_SHRINK OFF 
+GO
+ALTER DATABASE [Team Management] SET AUTO_UPDATE_STATISTICS ON 
+GO
+ALTER DATABASE [Team Management] SET CURSOR_CLOSE_ON_COMMIT OFF 
+GO
+ALTER DATABASE [Team Management] SET CURSOR_DEFAULT  GLOBAL 
+GO
+ALTER DATABASE [Team Management] SET CONCAT_NULL_YIELDS_NULL OFF 
+GO
+ALTER DATABASE [Team Management] SET NUMERIC_ROUNDABORT OFF 
+GO
+ALTER DATABASE [Team Management] SET QUOTED_IDENTIFIER OFF 
+GO
+ALTER DATABASE [Team Management] SET RECURSIVE_TRIGGERS OFF 
+GO
+ALTER DATABASE [Team Management] SET  DISABLE_BROKER 
+GO
+ALTER DATABASE [Team Management] SET AUTO_UPDATE_STATISTICS_ASYNC OFF 
+GO
+ALTER DATABASE [Team Management] SET DATE_CORRELATION_OPTIMIZATION OFF 
+GO
+ALTER DATABASE [Team Management] SET TRUSTWORTHY OFF 
+GO
+ALTER DATABASE [Team Management] SET ALLOW_SNAPSHOT_ISOLATION OFF 
+GO
+ALTER DATABASE [Team Management] SET PARAMETERIZATION SIMPLE 
+GO
+ALTER DATABASE [Team Management] SET READ_COMMITTED_SNAPSHOT OFF 
+GO
+ALTER DATABASE [Team Management] SET HONOR_BROKER_PRIORITY OFF 
+GO
+ALTER DATABASE [Team Management] SET RECOVERY SIMPLE 
+GO
+ALTER DATABASE [Team Management] SET  MULTI_USER 
+GO
+ALTER DATABASE [Team Management] SET PAGE_VERIFY CHECKSUM  
+GO
+ALTER DATABASE [Team Management] SET DB_CHAINING OFF 
+GO
+ALTER DATABASE [Team Management] SET FILESTREAM( NON_TRANSACTED_ACCESS = OFF ) 
+GO
+ALTER DATABASE [Team Management] SET TARGET_RECOVERY_TIME = 60 SECONDS 
+GO
+ALTER DATABASE [Team Management] SET DELAYED_DURABILITY = DISABLED 
+GO
+USE [Team Management]
+GO
+/****** Object:  Table [dbo].[Users]    Script Date: 7/5/2021 2:54:14 PM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Users](
+	[Id] [int] NOT NULL,
+	[Password] [nchar](10) NOT NULL,
+	[Username] [nchar](10) NOT NULL,
+	[First Name] [nchar](10) NOT NULL,
+	[Last Name] [nchar](10) NOT NULL,
+	[Date of creation] [datetime] NOT NULL,
+	[Creator Id] [int] NOT NULL,
+	[Date of change] [datetime] NOT NULL,
+	[Change Id] [int] NOT NULL
+) ON [PRIMARY]
+GO
+USE [master]
+GO
+ALTER DATABASE [Team Management] SET  READ_WRITE 
+GO
