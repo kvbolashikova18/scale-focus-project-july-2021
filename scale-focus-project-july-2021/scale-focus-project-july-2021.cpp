bb26cac24c0f952e55924550ce04edfb60521f2b
kvbolashikova18 <kvbolashikova18@codingburgas.bg>
2021-07-07 13:01:52 +0300

    Connect project to database

--- scale-focus-project-july-2021/scale-focus-project-july-2021.cpp
+++ scale-focus-project-july-2021/scale-focus-project-july-2021.cpp
@@ -1,8 +1,27 @@
-#include <iostream>
+#include <iostream> 
+#include <nanodbc.h> 
+#include <exception> 
+using namespace std;
+
+
 
 int main()
 {
-   
+	try {
+		auto const connstr = NANODBC_TEXT("DRIVER={ODBC Driver 17 for SQL Server};SERVER=(localdb)\\MSSQLLocaldb;DATABASE=BikeStores;Trusted_Connection=yes;"); // an ODBC connection string to your database nanodbc::connection conn(connstr);
+
+		nanodbc::connection conn(connstr);
+
+		auto result = nanodbc::execute(conn, NANODBC_TEXT(R"( SELECT TOP 2 * FROM BikeStores.sales.customers )"));
+
+		while (result.next()) {
+			auto i = result.get<int>(0); auto firstName = result.get<nanodbc::string>(1, "NULL FIELD");
+
+			cout << i << ", " << firstName << endl;
+		} return EXIT_SUCCESS;
+	}
+	catch (std::exception& e)
+	{
+		std::cerr << e.what() << std::endl; return EXIT_FAILURE;
+	}
 }
-
-
